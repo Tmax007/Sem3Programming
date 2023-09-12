0fad4f1d6279d1353e64303af7eaa0cca5ce4f25
Aditya Vashishtha <avtmax007@gmail.com>
2023-09-11 21:04:36 -0400

    Added bomb line

--- Assignment1-Unity-Template/Assets/Scripts/Components/BombLine.cs
+++ Assignment1-Unity-Template/Assets/Scripts/Components/BombLine.cs
@@ -8,13 +8,38 @@ public class BombLine : MonoBehaviour
     public int BombCount;
     public float BombSpacing;
 
+    void Update()
+    {
+        //Press B to spawn bombs
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnBombs();
+        }
+    }
     /// <summary>
     /// Spawn a line of instantiated BombPrefabs behind the player ship. There should be BombCount bombs placed with BombSpacing amount of space between them.
     /// </summary>
     /// <returns>An array containing all the bomb objects</returns>
     public GameObject[] SpawnBombs()
     {
-        return null;
+        GameObject[] bombArray = new GameObject[BombCount];
+
+        // Player's position
+        Vector3 playerPosition = transform.position;
+
+        // Initial position behind player
+        Vector3 initialPosition = playerPosition - transform.up * BombSpacing;
+
+        // Instantiate and store bomb objects
+        for (int i = 0; i < BombCount; i++)
+        {
+            // Position of the current bomb in line
+            Vector3 bombPosition = initialPosition - transform.up * i * BombSpacing;
+
+            // Instantiate bomb prefab at calculated position
+            bombArray[i] = Instantiate(BombPrefab, bombPosition, Quaternion.identity);
+        }
+
+        return bombArray;
     }
-    
 }
